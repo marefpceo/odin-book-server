3f7c2f1ced8ec2ee94000b4e02fc17cf52e50b7f
marefpceo <32721274+marefpceo@users.noreply.github.com>
2026-02-18 16:10:03 -0800

    Created userController and userRouter

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,32 @@
 // Handles getting a list of all users
-async function usersGet(req, res, next) {
+async function usersGet(req, res) {
   res.json({ title: 'GET /users' });
 }
 
-export default { usersGet };
+// Handles getting record for the selected user by userId
+async function selectedUserGet(req, res) {
+  res.json({ title: 'GET /users/:userId' });
+}
+
+// POST request to add selected user as a friend
+async function addSelectedUser(req, res) {
+  res.json({ title: 'POST /users/:userId/add' });
+}
+
+// PUT request to update friendship status
+async function updateFriendshipStatus(req, res) {
+  res.json({ title: 'PUT /users/:userId/update' });
+}
+
+// Handles DELETE request to remove selected user from friend's list
+async function removeFriend(req, res) {
+  res.json({ title: 'DELETE /users/:userId/remove' });
+}
+
+export default {
+  usersGet,
+  selectedUserGet,
+  addSelectedUser,
+  updateFriendshipStatus,
+  removeFriend,
+};
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -5,4 +5,16 @@ const router = express.Router();
 // GET users
 router.get('/', userController.usersGet);
 
+// GET selected user
+router.get('/:userId', userController.selectedUserGet);
+
+// POST add selected user
+router.post('/:userId/add', userController.addSelectedUser);
+
+// PUT update selected user's friendship status
+router.put('/:userId/update', userController.updateFriendshipStatus);
+
+// DELETE from friend list
+router.delete('/:userId/remove', userController.removeFriend);
+
 export default router;
