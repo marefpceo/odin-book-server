4a74ae0aed15e6ffeb7917c75384c90b3518d4c6
marefpceo <marefpceo@gmail.com>
2026-02-23 21:29:56 -0800

    Installed Express Validator and setup login route

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,31 +1,44 @@
 import passport from 'passport';
+import { body, validationResult } from 'express-validator';
 
 // Handles user signup
 async function signupPost(req, res) {
   res.json({ title: 'Signup Route' });
 }
 
-// Handles login
+// Handles user login
 async function loginPost(req, res, next) {
-  passport.authenticate('local', (err, user, info) => {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      return res.status(401).json({
-        message: info.message,
-      });
-    }
-    req.login(user, (err) => {
+  (body('email').trim().isEmail().escape(),
+    body('password').trim().isLength({ min: 9 }).escape(),
+    (req, res) => {
+      const error = validationResult(req);
+      if (!error.isEmpty()) {
+        res.status(401).json({
+          message: 'Email or password incorrect',
+          email: req.body.email,
+          password: req.body.password,
+        });
+      }
+    },
+    passport.authenticate('local', (err, user, info) => {
       if (err) {
         return next(err);
       }
-      res.status(200).json({
-        message: 'Login successful',
-        user: req.user,
+      if (!user) {
+        return res.status(401).json({
+          message: info.message,
+        });
+      }
+      req.login(user, (err) => {
+        if (err) {
+          return next(err);
+        }
+        res.status(200).json({
+          message: 'Login successful',
+          user: req.user,
+        });
       });
-    });
-  })(req, res, next);
+    })(req, res, next));
 }
 
 // Handles logout
