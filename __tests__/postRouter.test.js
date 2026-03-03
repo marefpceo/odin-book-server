5acd62ade750884515683f4747a3179af0b2eab1
marefpceo <marefpceo@gmail.com>
2026-03-02 18:40:08 -0800

    Creaed postRouter text file

---
+++ __tests__/postRouter.test.js
@@ -0,0 +1,17 @@
+import app from '../app';
+import { describe, test, expect } from 'vitest';
+import postRouter from '../routers/postRouter.js';
+
+import { PrismaClient } from '../prisma/generated/prisma/client.ts';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+import request from 'supertest';
+import express from 'express';
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
+
+app.use(express.urlencoded({ extended: false }));
+app.use('/', postRouter);
+
+describe('Test post routes', async () => {});
