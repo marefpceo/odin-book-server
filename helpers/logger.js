a9885a40c3c8439257eef955cd5e2d6383fd37f4
marefpceo <32721274+marefpceo@users.noreply.github.com>
2026-04-10 11:39:37 -0700

    Loggers installed and configured

--- helpers/logger.js
+++ helpers/logger.js
@@ -1,15 +1,24 @@
 import pino from 'pino';
 
 const transport = pino.transport({
-  target: 'pino/file',
-  options: { destination: './logs.log' },
+  // target: 'pino/file',
+  // options: { destination: './logs.log' },
+  target: 'pino-roll',
+  options: {
+    file: './logs/log.log',
+    frequency: 'weekly',
+    size: '20MB',
+    mkdir: true,
+    symlink: true,
+    limit: { count: 3 },
+  },
   level: process.env.NODE_ENV === 'production' ? 'error' : 'debug',
 });
 const logger = pino(
   {
     timestamp: pino.stdTimeFunctions.isoTime,
     formatters: {
-      level(label, number) {
+      level(label) {
         return { level: label };
       },
     },
