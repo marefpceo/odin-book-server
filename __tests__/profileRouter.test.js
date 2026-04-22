c133ed658932805329d4fd2caf594fd351990b23
marefpceo <32721274+marefpceo@users.noreply.github.com>
2026-04-22 15:50:12 -0700

    Removed tests no longer needed

--- __tests__/profileRouter.test.js
+++ __tests__/profileRouter.test.js
@@ -60,18 +60,6 @@ describe('Test all routes in profileRouter', async () => {
   const profileNoId = katieUserId.profile === null ? 0 : katieUserId.profile.id;
   const profileId = jamesUserId.profile === null ? 0 : jamesUserId.profile.id;
 
-  test('GET profile route for a user with NO profile', async () => {
-    verifyValidSession.mockImplementation((req, res, next) => {
-      req.session.passport = { id: 1, role: 'USER' };
-      next();
-    });
-
-    const res = await request(app).get(`/profile/${profileNoId}`);
-
-    expect(res.statusCode).toEqual(200);
-    expect(res.body.message).toEqual('User has not created a profile');
-  });
-
   test('GET profile route for a user with profile', async () => {
     verifyValidSession.mockImplementation((req, res, next) => {
       req.session.passport = { id: 1, role: 'USER' };
@@ -91,24 +79,6 @@ describe('Test all routes in profileRouter', async () => {
       createdAt: expect.any(String),
       updatedAt: expect.any(String),
     });
-  });
-
-  test('POST call to create a profile for the logged in user', async () => {
-    verifyValidSession.mockImplementation((req, res, next) => {
-      req.session.passport = { id: 1, role: 'USER' };
-      next();
-    });
-
-    const res = await request(app)
-      .post(`/profile/create`)
-      .field('userId', katieUserId.id)
-      .field('firstname', 'Kate')
-      .field('lastname', 'Did')
-      .field('bio', 'Everyone knows Katie Did')
-      .attach('avatar', testFilePath);
-
-    expect(res.status).toEqual(200);
-    expect(res.body.message).toEqual('Profile successfully created!');
   });
 
   test('PUT call to update profile for the logged in user', async () => {
@@ -139,36 +109,5 @@ describe('Test all routes in profileRouter', async () => {
 
     expect(res.status).toEqual(200);
     expect(res.body.message).toEqual('Profile successfully updated!');
-  });
-
-  describe('test delete route with actual login data', () => {
-    let kateRes;
-    let cookies;
-    let profileId;
-
-    beforeAll(async () => {
-      kateRes = await request.agent(app).post(`/auth/login`).send({
-        email: 'kate@test.com',
-        password: 'kkkkkkkkk',
-      });
-
-      cookies = kateRes.headers['set-cookie'];
-      profileId = kateRes.body.user.profile;
-    });
-
-    test('deleting profile for logged in user', async () => {
-      verifyValidSession.mockImplementation((req, res, next) => {
-        req.session.passport.user = { id: kateRes.body.user.id, role: 'USER' };
-        next();
-      });
-
-      const res = await request
-        .agent(app)
-        .delete(`/profile/${profileId}/delete`)
-        .set('Cookie', cookies);
-
-      expect(res.status).toEqual(200);
-      expect(res.body.message).toEqual('Profile deleted');
-    });
   });
 });
